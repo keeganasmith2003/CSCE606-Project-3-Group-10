f199868dbb34cd40dd109f6d7c16d1acd6c084da
JohnMcWhirter10 <jmcwhirter1608@tamu.edu>
2025-11-26 16:42:27 -0600

    TB-106: Bracket Mode Toggle
    Creating a toggle to distinguish between bracket mode 'Draft' and 'Active'. Creating a feature test for visual of the bracket mode toggle.

--- TournamentEditor/app/controllers/application_controller.rb
+++ TournamentEditor/app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html
-      format.csv { send_data @users.to_csv, filename: "tournament-#{Date.today}.csv" }
-    end
   end
 end
